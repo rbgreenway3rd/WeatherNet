7e0a08601f19e467d8f2fc9356b3b56f23b0c701
rbgreenway3rd <3rdgreenway@gmail.com>
2021-06-15 11:27:01 -0500

    re-ordered files for a better workflow. Updated index.js and WeatherNet.js to render welcome text.

---
+++ src/components/WeatherNet.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./WeatherNet.css";
+
+export const WeatherNet = () => (
+  <>
+    <h2>WeatherNet</h2>
+    <small>SkyNet's benevolent cousin</small>
+    <aside>
+      <div>To get started, register a profile.</div>
+      <div>From there, provide a location and get the forecast!</div>
+    </aside>
+  </>
+);
